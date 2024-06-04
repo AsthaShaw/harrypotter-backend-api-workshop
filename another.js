50a9744618e67a08b4bf992d0e27e1e1650ba8f2
asthashaw <astha.shaw100@gmail.com>
2024-06-04 10:16:26 +0100

    Add object destructuring and mike patch example

---
+++ another.js
@@ -0,0 +1,12 @@
+const student = {
+    name: "Mrinal",
+    age: 18,
+    gender: "Male"
+}
+
+function printPerson({ name, age, gender }, course) {
+    console.log(`The person's name is ${name} , age is ${age}, gender is ${gender} and has renrolled to the course ${course}`)
+
+}
+
+printPerson(student, "Physics");
